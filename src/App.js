f0f629803774678ebda4cf8c7acc678e5efd4ad8
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-07-02 15:54:04 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dominik Piłat</h1>
       </header>
+      <main>
+        <p>This will be the main content</p>
+      </main>
+      <footer>
+        The footer
+      </footer>
     </div>
   );
 }
