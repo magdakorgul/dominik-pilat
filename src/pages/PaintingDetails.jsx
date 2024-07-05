2f4bd6acf6f6e7c3f6da797c64ea5056067757b5
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-07-05 13:52:20 +0200

    Style painting details page grid

--- src/pages/PaintingDetails.jsx
+++ src/pages/PaintingDetails.jsx
@@ -26,15 +26,22 @@ const PaintingDetails = () => {
         <div className="painting-details">
             <div key={painting.id} className="item">
                     
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-2 gap-4 ml-12 mr-14 mt-28 mb-14">
                         <div>
-                        <img src={painting.imageUrl} alt={painting.title} />
+                        <img src={painting.imageUrl} alt={painting.title} className="w-11/12 h-11/12 mr-14" />
                         </div>
-                        <div>
-                    <p>{painting.title}</p>
-                    <p>{painting.price} €</p>
-                    <p>{painting.description}</p>
-                    <p>{painting.details}</p>
+                        <div >
+                    <h1 className="text text-left text-3xl">{painting.title}</h1>
+                    <h2 className="text text-left text-xl">{painting.price} €</h2>
+                    <p className="text text-left text-base mt-8 mb-4 leading-tight">{painting.description}</p>
+                    <button type="button" className="inline-flex items-center justify-center rounded-md">
+
+                    </button>
+                    <p className="text text-left text-base">Details <br /></p>
+                    <ul className="text text-left text-base list-disc pl-4">
+                        {painting.details.map((detail, index) => (
+                            <li key={index}>{detail}</li>
+                        ))}</ul>
                     </div>
                     </div>
                 </div>
