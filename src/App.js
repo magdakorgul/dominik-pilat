bb124c5f06c2fe6b06aa29c426feb6c99d5d1629
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-07-23 11:53:57 +0200

    Adjust colors to separate subpages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Paintings from './pages/Paintings';
 import PaintingDetails from './pages/PaintingDetails';
+// import LargeGrid from './components/LargeGrid'; 
 
-function App() {
-  return (
+
+
+const App = () => {
+  const location = useLocation();
+
+  console.log('Current pathname:', location.pathname);
+
+  const getClassForPath = (pathname) => {
+    if (pathname === '/paintings') {
+      return 'bg-paintings';
+    } else if (pathname.match(/^\/paintings\/\d+$/)) { // bardziej precyzyjne sprawdzenie dynamicznej ścieżki
+      return 'bg-painting-details';
+    } else {
+      return '';
+    }
+  };
+
+    const currentClass = getClassForPath(location.pathname);
+    console.log('Assigned class:', currentClass); // Dodaj logowanie przypisanej klasy
+
+
+
+  // const backgroundClasses = {
+  //   "/paintings": "bg-paintings",
+  //   "/paintings/:paintingId": "bg-painting-details",
     
-    <Router>
-      {/* <Navbar /> */}
-      <div className="App">
-        <Routes>
-          {/* <Route path="/" /> */}
-          <Route path="/paintings" element={<Paintings />} />
-          <Route path="/paintings/:paintingId" element={<PaintingDetails />} />
-        </Routes>
-      </div>
+  // };
+
+  // const currentBackgroundClass = backgroundClasses[window.location.pathname] || "";
+
+  return (
+    <div className={`App ${currentClass}`}>     
+     <Routes>
+        
+        <Route path="/paintings" element={<Paintings />}/>
+        <Route path="/paintings/:paintingId" element={<PaintingDetails />} />
+        
+        {/* Dodaj więcej tras w razie potrzeby */}
+      </Routes>
       <footer>
         The footer
       </footer>
-      </Router>
+    </div>
   );
-}
+};
+
 
 export default App;
--- src/components/LargeGrid.jsx
+++ src/components/LargeGrid.jsx
@@ -3,15 +3,16 @@ import {Link} from "react-router-dom";
 
 
 const LargeGrid = ({items}) => {
+    console.log('Items:', items);
+
 
     return(
 
         <div className="large-grid grid grid-cols-4 p-8 gap-x-0 gap-5">
             {items.map(item => (
-                <div key={item.id} className="item">
+                <div key={item.id} className="item paintings-card">
                     <Link to={`/paintings/${item.id}`} className="no-underline">
-                    <img src={item.imageUrl} alt={item.title} className="image w-72 h-72 mb-0" />
-                    </Link>
+                    <img src={item.imageUrl} alt={item.title} className="image w-72 h-72 mb-2" />
                     <div className="flex justify-between w-72">
                     <p className="title text-left text-xl mb-0">{item.title}</p>
                     <p className="text-base text-right mb-0">
@@ -23,6 +24,7 @@ const LargeGrid = ({items}) => {
                             )}
                             </p>
                     </div>
+                    </Link>
                 </div>
             ))}
 
