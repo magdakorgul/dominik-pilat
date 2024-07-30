ad201c36ede8fb73918283bb9f4fc209e2e26d3e
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-07-30 12:45:49 +0200

    Style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,28 +5,28 @@ const Navbar = ({backgroundClass}) => {
 
 return(
 <nav className={`navbar ${backgroundClass}`}>
-  <div className="max-w-screen-xl flex flex-wrap  justify-between mx-auto pb-1 pt-0 text-transform: uppercase">
-    <a href="https://google.com/" className="flex space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+<div className="max-w-screen-xl ml-auto mr-auto flex items-center justify-between pb-1 pt-0 text-transform: uppercase">
+    <a href="/home">
         <span className="self-center text-2xl font-semibold whitespace-nowrap">Dominik Piłat</span>
     </a>
-   
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-    <ul className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg ${backgroundClass} md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0`}>
-  <li>
-    <a href="/design" className="block py-2 px-2 text-white rounded md:text-white md:p-0" aria-current="page">Design</a>
+        <ul className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg ${backgroundClass} md:flex-row md:space-x-2 rtl:space-x-reverse md:mt-0 md:border-0`}>
+
+
+    <li>
+    <a href="/design" className="block  text-gray-900 rounded  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0" aria-current="page">Design</a>
   </li>
   <li>
-    <a href="/paintings" className="block py-2 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Paintings</a>
+    <a href="/paintings" className="block  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Paintings</a>
   </li>
   <li>
-    <a href="/branding" className="block py-2 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Branding</a>
+    <a href="/branding" className="block  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Branding</a>
   </li>
   <li>
-    <a href="/about" className="block py-2 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">About</a>
+    <a href="/about" className="block  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">About</a>
   </li>
-  <li>
-    <a href="/contact" className="block py-2 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Contact</a>
+  <li className="pr-0">
+    <a href="/contact" className="block  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Contact</a>
   </li>
 </ul>
 
