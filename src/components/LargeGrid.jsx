fa00af3a809c2c18ae0b45f010f8c68633aea9ab
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-08-22 12:32:38 +0200

    Large grid spacing adjusting for responsive

--- src/components/LargeGrid.jsx
+++ src/components/LargeGrid.jsx
@@ -8,15 +8,17 @@ const LargeGrid = ({items, basePath, showPrice}) => {
 
     return(
 
-        <div className="large-grid grid grid-cols-4 gap-x-8 gap-1 mx-32 mt-16 uppercase max-w-screen-xl">
+        <div className="large-grid grid grid-cols-2 mx-6 gap-x-2
+        md:grid-cols-4 md:gap-x-8 md:gap-1 md:mx-32 md:mt-16 uppercase md:max-w-screen-xl">
             {items.map(item => (
                 <div key={item.id} className="item paintings-card">
-                    <Link to={`/${basePath}/${item.id}`} className="no-underline">
-                    <img src={item.imageUrl} alt={item.title} className="image w-64 h-64 mb-2" />
-                    <div className="flex justify-between w-64 pt-2">
-                    <p className="title text-left text-xl m-0">{item.title}</p>
+                    <Link to={`/${basePath}/${item.id}`} className="no-underline block">
+                    <img src={item.imageUrl} alt={item.title} className="image w-44 h-44 mb-1 px-2 block
+                     md:w-64 md:h-64 md:mb-2" />
+                    <div className="flex justify-between md:w-64 pt-2">
+                    <p className="title text-left md:text-xl m-0 hidden md:block lg:block">{item.title}</p>
                     {showPrice && (
-                    <p className="text-base text-right m-0">
+                    <p className="text-base text-right m-0 hidden md:block lg:block">
                             {item.price} 
                             {item.price !== "sold" && (
                                 <>
