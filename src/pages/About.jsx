4847e9dc76304cea54c2bbd03f6f3feb19e2ad54
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-08-23 12:17:44 +0200

    Responsive about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,46 +1,48 @@
 import { useEffect, useState } from 'react';
 import "../../src/App.css";
-import {ReactComponent as Behance} from "../assets/behance.svg";
-import {ReactComponent as Instagram} from "../assets/instagram.svg";
-import {ReactComponent as Mail} from "../assets/mail.svg";
-import {ReactComponent as Linkedin} from "../assets/linkedin.svg";
-import {Link} from "react-router-dom";
+import { ReactComponent as Behance } from "../assets/behance.svg";
+import { ReactComponent as Instagram } from "../assets/instagram.svg";
+import { ReactComponent as Mail } from "../assets/mail.svg";
+import { ReactComponent as Linkedin } from "../assets/linkedin.svg";
+import { Link } from "react-router-dom";
 
 const About = () => {
     return (
         <div className="about-details">
-            <div className="grid grid-cols-2 gap-0 ml-24 mr-24 mt-16 mb-14 mx-32">
+            <div className="grid grid-cols-1 mx-8 mt-4
+            md:grid-cols-2 md:gap-0 md:ml-24 md:mr-24 md:mt-16 md:mb-14 md:mx-32">
                 <div className="max-w-lg">
-                    <h1 className="text-left text-3xl uppercase">Dominik Pilat</h1>
+                    <div className="flex justify-center mb-6 md:hidden">
+                        <img src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg" alt="dominik" className="max-w-full h-auto" />
+                    </div>
+                    <h1 className="text-left text-xl font-semibold md:text-3xl uppercase">Dominik Pilat</h1>
                     <p className="text-left text-base mt-8 mb-4 leading-tight">
                         Mauris id augue eu ligula mollis convallis nec at ligula. Praesent pharetra leo erat, et laoreet lacus mattis mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce placerat, orci vitae sollicitudin lacinia, justo massa ultrices nibh, at commodo mauris libero vitae mauris.
                         <br /><br />
                         Mauris id augue eu ligula mollis convallis nec at ligula. Praesent pharetra leo erat, et laoreet lacus mattis mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce placerat, orci vitae sollicitudin lacinia, justo massa ultrices nibh, at commodo mauris libero vitae mauris.
                         <br /><br />
-                        <p className="font-bold text-left">Enquiries</p>
+                        <p className="font-bold text-left mb-0">Enquiries</p>
                         <br />dominic.pilat@gmail.com 
                         <br />instagram
                     </p>
-                    <div className="flex items-center space-x-4">
-                    <Link to="https://www.behance.net/dominikpilat" target="_blank" className="w-8 h-8" style={{ marginLeft: '-4px', marginRight: '-4px'}}>
-                        <Behance className="w-full h-full inline-block"/>
-                    </Link>
-                    <Link to="https://www.instagram.com/dominik_pilat" target="_blank">
-                        <Instagram className="w-full h-full"/>
-                    </Link>
-                    <Link to='javascript:void(0)' target="_blank"
-                    onClick={() => window.location = 'mailto:pilatdom@gmail.com'}>
-                        <Mail className="w-full h-full"/>
-                    </Link>
-                    <Link to="https://www.linkedin.com" target="_blank">
-                        <Linkedin className="w-full h-full"/>
-                    </Link>
-
-
-
+                    {/* Dodanie justify-center */}
+                    <div className="flex justify-center items-center space-x-8 md:space-x-4 mt-12 mb-6">
+                        <Link to="https://www.behance.net/dominikpilat" target="_blank" className="w-8 h-8">
+                            <Behance className="w-full h-full inline-block"/>
+                        </Link>
+                        <Link to="https://www.instagram.com/dominik_pilat" target="_blank">
+                            <Instagram className="w-full h-full"/>
+                        </Link>
+                        <Link to='javascript:void(0)' target="_blank"
+                            onClick={() => window.location = 'mailto:pilatdom@gmail.com'}>
+                            <Mail className="w-full h-full"/>
+                        </Link>
+                        <Link to="https://www.linkedin.com" target="_blank">
+                            <Linkedin className="w-full h-full"/>
+                        </Link>
                     </div>
                 </div>
-                <div className="flex justify-center">
+                <div className="hidden md:flex md:justify-center md:mb-0">
                     <img src="https://ralfvanveen.com/wp-content/uploads/2021/06/Placeholder-_-Glossary.svg" alt="dominik" className="max-w-full h-auto" />
                 </div>
             </div>
