7b0fcda9945a3199d63140983ae6c8cf1397aeeb
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-08-26 12:54:07 +0200

    Navbar styling responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,172 @@
+import React, { useState } from "react";
 import "../App.css";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Behance } from "../assets/behance.svg";
+import { ReactComponent as Instagram } from "../assets/instagram.svg";
+import { ReactComponent as Mail } from "../assets/mail.svg";
+import { ReactComponent as Linkedin } from "../assets/linkedin.svg";
+import { Link } from "react-router-dom";
 
-const Navbar = ({backgroundClass}) => {
+const Navbar = ({ backgroundClass }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
-return(
-<nav className={`navbar ${backgroundClass}`}>
-<div className="max-w-screen-xl ml-auto mr-auto flex items-center justify-between pt-0 pb-6 text-transform: uppercase">
-    <a href="/home">
-        <span className="self-center text-2xl font-semibold whitespace-nowrap">Dominik Piłat</span>
-    </a>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg ${backgroundClass} md:flex-row md:space-x-2 rtl:space-x-reverse md:mt-0 md:border-0`}>
+  return (
+    <>
+      <nav className={`navbar ${backgroundClass} px-4`}>
+        <div className="max-w-screen-xl mx-auto flex items-center justify-between py-6 uppercase md:mx-32">
+          <a href="/home" className="text-2xl font-semibold whitespace-nowrap text-white">
+            Dominik Piłat
+          </a>
 
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-white rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 flex-shrink-0 ml-auto"
+            aria-controls="navbar-default"
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            {isMenuOpen ? (
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            )}
+          </button>
 
-    <li>
-    <a href="/design" className="block  text-white rounded  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0" aria-current="page">Design</a>
-  </li>
-  <li>
-    <a href="/paintings" className="block  text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Paintings</a>
-  </li>
-  <li>
-    <a href="/branding" className="block  text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Branding</a>
-  </li>
-  <li>
-    <a href="/about" className="block  text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">About</a>
-  </li>
-  <li className="pr-0">
-    <a href="/contact" className="block  text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0">Contact</a>
-  </li>
-</ul>
+          <div
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } md:flex md:items-center md:w-auto fixed inset-0 bg-gray-800 z-50 flex-col justify-center md:relative md:bg-transparent md:z-auto`}
+            id="navbar-default"
+          >
+            {/* Znak X */}
+            <button
+              onClick={toggleMenu}
+              className="absolute top-4 right-4 text-white hover:text-gray-400 focus:outline-none md:hidden"
+            >
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
 
-    </div>
-  </div>
-</nav>
-)
+            <ul className="flex flex-col items-center justify-center w-full h-full p-4 mt-4 text-center space-y-8 md:flex-row md:space-x-6 md:mt-0 md:space-y-0 md:p-0">
+              <li>
+                <NavLink
+                  to="/design"
+                  className={({ isActive }) =>
+                    `block text-white text-3xl md:text-base ${
+                      isActive ? "underline" : ""
+                    } hover:text-gray-400`
+                  }
+                  onClick={toggleMenu}
+                >
+                  Design
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/paintings"
+                  className={({ isActive }) =>
+                    `block text-white text-3xl md:text-base ${
+                      isActive ? "underline" : ""
+                    } hover:text-gray-400`
+                  }
+                  onClick={toggleMenu}
+                >
+                  Paintings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/branding"
+                  className={({ isActive }) =>
+                    `block text-white text-3xl md:text-base ${
+                      isActive ? "underline" : ""
+                    } hover:text-gray-400`
+                  }
+                  onClick={toggleMenu}
+                >
+                  Branding
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `block text-white text-3xl md:text-base ${
+                      isActive ? "underline" : ""
+                    } hover:text-gray-400`
+                  }
+                  onClick={toggleMenu}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `block text-white text-3xl md:text-base ${
+                      isActive ? "underline" : ""
+                    } hover:text-gray-400`
+                  }
+                  onClick={toggleMenu}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
 
+            {/* Ikony mediów społecznościowych */}
+            <div className="absolute bottom-4 flex justify-center space-x-4 md:hidden">
+              <Link to="https://www.behance.net/dominikpilat" target="_blank" className="w-8 h-8">
+                <Behance className="w-full h-full inline-block"/>
+              </Link>
+              <Link to="https://www.instagram.com/dominik_pilat" target="_blank">
+                <Instagram className="w-full h-full"/>
+              </Link>
+              <Link to='javascript:void(0)' target="_blank"
+                  onClick={() => window.location = 'mailto:pilatdom@gmail.com'}>
+                <Mail className="w-full h-full"/>
+              </Link>
+              <Link to="https://www.linkedin.com" target="_blank">
+                <Linkedin className="w-full h-full"/>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
+
 export default Navbar;
