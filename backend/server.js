a83be72e9cb6f845786f59dbcd47522a35a9d22d
Magdalena Korgul <magdalenakorgul1@gmail.com>
2024-09-19 21:45:32 +0200

    Add the possibility to send an email through the form

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+
+console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+
+
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
@@ -10,35 +16,43 @@ app.use(cors());
 app.use(express.json());
 
 // Endpoint do obsługi formularza
-app.post('/send-email', (req, res) => {
-  const { email, subject, message } = req.body;
-
-  // Konfiguracja transportera nodemailer
-  const transporter = nodemailer.createTransport({
-    service: 'gmail', // lub inny serwis
-    auth: {
-      user: 'twoj_email@gmail.com',
-      pass: 'twoje_haslo'
-    }
-  });
-
-  const mailOptions = {
-    from: email,
-    to: 'twoj_email@gmail.com',
-    subject: `Wiadomość od ${email}: ${subject}`,
-    text: message
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Wystąpił błąd podczas wysyłania wiadomości.');
-    } else {
+app.post('/send-email', async (req, res) => {
+    try {
+      const { email, subject, message } = req.body;
+  
+      // Konfiguracja transportera
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER, // Twój adres email
+          pass: process.env.EMAIL_PASS,  // Wygenerowane hasło aplikacji
+          logger: true,
+          debug: true
+        }
+      });
+  
+      const mailOptions = {
+        from: process.env.EMAIL_USER, // Must be your authenticated Gmail address
+        to: process.env.EMAIL_USER, // Sending to your own email
+        subject: subject,
+        text: `You have received a new message from ${email}:\n\n${message}`,
+        replyTo: email, // This sets the user's email as the reply-to address
+      };
+  
+      // Wysyłanie maila
+      const info = await transporter.sendMail(mailOptions);
       console.log('Email sent: ' + info.response);
-      res.status(200).send('Wiadomość została wysłana pomyślnie.');
+  
+      // Zwróć odpowiedź sukcesu
+      res.status(200).json({ success: true, message: 'Email was sent successfully!' });
+    } catch (error) {
+      console.error('Error while sending email', error);
+  
+      // Zwróć odpowiedź błędu
+      res.status(500).json({ success: false, message: 'There was an error while sending email', error: error.message });
     }
   });
-});
+  
 
 app.listen(PORT, () => {
   console.log(`Serwer działa na porcie ${PORT}`);
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,122 +4,192 @@ import { ReactComponent as Instagram } from "../assets/instagram.svg";
 import { ReactComponent as Mail } from "../assets/mail.svg";
 import { ReactComponent as Linkedin } from "../assets/linkedin.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Contact = () => {
-    return (
-        <div className="contact grid grid-cols-1 gap-4 mt-16 mb-0 mx-8 text-left md:grid-cols-2 md:mx-32 md:mb-0">
-            <div className="flex flex-col text-left">
-                <div className="flex flex-col items-start justify-start">
-                    <h1 className="uppercase text-left font-semibold text-xl">Contact</h1>
-                    <form className="w-full md:w-4/5 mt-4">
-                        
-                        <div className="relative z-0 w-full mb-5 group">
-                            <input
-                                type="email"
-                                name="floating_email"
-                                id="floating_email"
-                                className="block py-2.5 px-0 w-full text-sm text-white placeholder-[#D7C1AF] bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
-                                placeholder=" "
-                                required
-                            />
-                            <label
-                                htmlFor="floating_email"
-                                className="peer-focus:font-medium absolute text-sm text-[#D7C1AF] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                            >
-                                Your email
-                            </label>
-                        </div>
+  const [formData, setFormData] = useState({
+    email: "",
+    subject: "",
+    message: "",
+  });
 
-                       
-                        <div className="relative z-0 w-full mb-5 group">
-                            <input
-                                type="text"
-                                name="floating_short_text"
-                                id="floating_short_text"
-                                className="block py-2.5 px-0 w-full text-sm text-white placeholder-[#D7C1AF] bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
-                                placeholder=" "
-                                required
-                            />
-                            <label
-                                htmlFor="floating_short_text"
-                                className="peer-focus:font-medium absolute text-sm text-[#D7C1AF] duration-300 transform -translate-y-4 scale-90 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-90 peer-focus:-translate-y-4"
-                            >
-                                How can I help you?
-                            </label>
-                        </div>
+  const [loading, setLoading] = useState(false);
+  const [feedbackMessage, setFeedbackMessage] = useState("");
 
-                        
-                        <div className="relative z-0 w-full mb-5 group">
-                            <textarea
-                                name="floating_message"
-                                id="floating_message"
-                                rows="1"
-                                className="block py-2.5 px-0 w-full text-sm text-white placeholder-[#D7C1AF] bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer resize-none overflow-hidden"
-                                placeholder=" "
-                                required
-                                onInput={(e) => {
-                                    e.target.style.height = 'auto';
-                                    e.target.style.height = e.target.scrollHeight + 'px';
-                                }}
-                            />
-                            <label
-                                htmlFor="floating_message"
-                                className="peer-focus:font-medium absolute text-sm text-[#D7C1AF] duration-300 transform -translate-y-4 scale-90 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-90 peer-focus:-translate-y-4"
-                            >
-                                Your message
-                            </label>
-                        </div>
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-                        <div className="w-full mt-8 ">
-                            <button
-                                type="submit"
-                                className="text-black bg-white hover:bg-gray-100 font-medium rounded-sm text-sm w-full px-5 py-2.5 text-center"
-                            >
-                                Send
-                            </button>
-                        </div>
-                    </form>
-                </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setFeedbackMessage("");
 
-                <div className="mt-12">
-                    <p className="text-left mb-1 font-semibold">Enquiries</p>
-                    <a
-              href="mailto:dominic.pilat@gmail.com"
-              className="text-left mt-0 mb-1 underline block text-base leading-tight"
-            >
-              dominic.pilat@gmail.com
-            </a>
-            <a
-              href="https://www.instagram.com/dominikpilat"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-left mt-0 underline block text-base leading-tight"
-            >
-              instagram
-            </a>
-                </div>
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/send-email",
+        formData
+      );
 
-                <div className="flex justify-center md:justify-start items-center space-x-8 mt-44 md:mt-20 mb-6 md:mb-0">
-                    <Link to="https://www.behance.net/dominikpilat" target="_blank" className="w-8 h-8">
-                        <Behance className="w-full h-full inline-block" />
-                    </Link>
-                    <Link to="https://www.instagram.com/dominik_pilat" target="_blank">
-                        <Instagram className="w-full h-full" />
-                    </Link>
-                    <Link
-                        to="javascript:void(0)"
-                        target="_blank"
-                        onClick={() => (window.location = "mailto:pilatdom@gmail.com")}
-                    >
-                        <Mail className="w-full h-full" />
-                    </Link>
-                    <Link to="https://www.linkedin.com" target="_blank">
-                        <Linkedin className="w-full h-full" />
-                    </Link>
-                </div>
+      if (response.data.success) {
+        setFeedbackMessage("Email was sent successfully!");
+        // Wyczyść formularz
+        setFormData({
+          email: "",
+          subject: "",
+          message: "",
+        });
+      } else {
+        setFeedbackMessage(
+          "There was an error while sending email. Try again later."
+        );
+      }
+    } catch (error) {
+      console.error("Error while sending email:", error);
+      setFeedbackMessage(
+        "There was an error while sending email. Try again later."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="contact grid grid-cols-1 gap-4 mt-16 mb-0 mx-8 text-left md:grid-cols-2 md:mx-32 md:mb-0">
+      <div className="flex flex-col text-left">
+        <div className="flex flex-col items-start justify-start">
+          <h1 className="uppercase text-left font-semibold text-xl">Contact</h1>
+          <form className="w-full md:w-4/5 mt-4" onSubmit={handleSubmit}>
+            {/* Pole email */}
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="block py-2.5 px-0 w-full text-sm text-white placeholder-[#D7C1AF] bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="email"
+                className="peer-focus:font-medium absolute text-sm text-[#D7C1AF] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0]"
+              >
+                Your email
+              </label>
             </div>
+
+            {/* Pole temat */}
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                value={formData.subject}
+                onChange={handleChange}
+                className="block py-2.5 px-0 w-full text-sm text-white placeholder-[#D7C1AF] bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="subject"
+                className="peer-focus:font-medium absolute text-sm text-[#D7C1AF] duration-300 transform -translate-y-4 scale-90 top-3 -z-10 origin-[0]"
+              >
+                How can I help you?
+              </label>
+            </div>
+
+            {/* Pole wiadomość */}
+            <div className="relative z-0 w-full mb-5 group">
+              <textarea
+                name="message"
+                id="message"
+                value={formData.message}
+                onChange={handleChange}
+                rows="1"
+                className="block py-2.5 px-0 w-full text-sm text-white placeholder-[#D7C1AF] bg-transparent border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-white peer resize-none overflow-hidden"
+                placeholder=" "
+                required
+                onInput={(e) => {
+                  e.target.style.height = "auto";
+                  e.target.style.height = e.target.scrollHeight + "px";
+                }}
+              />
+              <label
+                htmlFor="message"
+                className="peer-focus:font-medium absolute text-sm text-[#D7C1AF] duration-300 transform -translate-y-4 scale-90 top-3 -z-10 origin-[0]"
+              >
+                Your message
+              </label>
+            </div>
+
+            {/* Komunikat zwrotny */}
+            {feedbackMessage && (
+              <div className="feedback-message mt-4">{feedbackMessage}</div>
+            )}
+
+            {/* Przycisk wyślij */}
+            <div className="w-full mt-8 ">
+              <button
+                type="submit"
+                className="text-black bg-white hover:bg-gray-100 font-medium rounded-sm text-sm w-full px-5 py-2.5 text-center"
+                disabled={loading}
+              >
+                {loading ? "Sending..." : "Send"}
+              </button>
+            </div>
+          </form>
         </div>
-    );
+
+        <div className="mt-12">
+          <p className="text-left mb-1 font-semibold">Enquiries</p>
+          <a
+            href="mailto:dominic.pilat@gmail.com"
+            className="text-left mt-0 mb-1 underline block text-base leading-tight"
+          >
+            dominic.pilat@gmail.com
+          </a>
+          <a
+            href="https://www.instagram.com/dominikpilat"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-left mt-0 underline block text-base leading-tight"
+          >
+            instagram
+          </a>
+        </div>
+
+        <div className="flex justify-center md:justify-start items-center space-x-8 mt-44 md:mt-20 mb-6 md:mb-0">
+          <Link
+            to="https://www.behance.net/dominikpilat"
+            target="_blank"
+            className="w-8 h-8"
+          >
+            <Behance className="w-full h-full inline-block" />
+          </Link>
+          <Link to="https://www.instagram.com/dominik_pilat" target="_blank">
+            <Instagram className="w-full h-full" />
+          </Link>
+          <a
+            href="mailto:dominic.pilat@gmail.com"
+            className="w-8 h-8"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Mail className="w-full h-full" />
+          </a>
+          <Link to="https://www.linkedin.com" target="_blank">
+            <Linkedin className="w-full h-full" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
