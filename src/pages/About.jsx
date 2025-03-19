bfba9ef7270867219351117ede3fa32816fb973f
Magdalena Korgul <magdalenakorgul1@gmail.com>
2025-03-19 15:04:16 +0100

    Change the description

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,12 +25,14 @@ const About = () => {
           </h1>
           <p className="text-left text-base mt-8 mb-4 leading-tight">
           Dominik Piłat is a Berlin-based artist whose work redefines geometric abstraction through a lens of figurative storytelling. With a distinctive style that merges bold, clean lines and vibrant color contrasts, Piłat’s compositions balance simplicity with depth, creating visual narratives that resonate with both clarity and emotional complexity.  
+          <br />
 
 Drawing inspiration from the rhythms of daily life and the people who surround him, Piłat transforms ordinary moments into extraordinary explorations of form and space. His artistic practice is deeply rooted in the principles of branding and design, where he honed the ability to distill complex ideas into singular, powerful forms. This foundation informs his work today, as he uses minimalist geometric shapes and restrained palettes to evoke layered stories and universal themes. 
 
             <br />
             <br />
             Piłat’s paintings are a celebration of the geometric, a playful yet refined investigation of balance, tension, and the interplay of shapes. His work invites viewers to engage with the spaces between forms, offering a fresh, contemporary perspective on abstraction. 
+            <br />
 
 Emerging from a modernist tradition yet distinctly contemporary, Piłat’s art is a testament to the enduring power of simplicity and the profound beauty of the everyday.
             <br />
